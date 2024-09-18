cea94878d49223359356a09c566a14562f4b8bfb
zhh375 <just0211@sina.com>
2024-09-19 01:35:33 +0800

    update

--- app/qianfan_api.py
+++ app/qianfan_api.py
@@ -1,22 +1,30 @@
-import os
-import qianfan
+import requests
+import json
 from config import QIANFAN_ACCESS_KEY, QIANFAN_SECRET_KEY
 
 
-os.environ["QIANFAN_ACCESS_KEY"] = QIANFAN_ACCESS_KEY
-os.environ["QIANFAN_SECRET_KEY"] = QIANFAN_SECRET_KEY
+def get_access_token():
+    url = ("https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={}&client_secret={}"
+           .format(QIANFAN_ACCESS_KEY, QIANFAN_SECRET_KEY))
+    payload = json.dumps("")
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.json().get("access_token")
 
 
 def qianfan_chat(word_list, type_chat=0):
+    url = ("https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions?access_token=" + get_access_token())
     if type_chat == 0:
         content = "用下面的汉字和笔画较少的汉字组一个不少于10个字且不超过20个字的短句：" + " ".join(word_list)
     else:
         content = "用下面的汉字和笔画较少的汉字组一个不超过50个字的故事：" + " ".join(word_list)
-    chat_comp = qianfan.ChatCompletion()
-    resp = chat_comp.do(model="ERNIE-3.5-8K", messages=[{
-        "role": "user",
-        "content": content
-    }]).body
+    payload = json.dumps({"messages": [{"role": "user", "content": content}]})
+    headers = {'Content-Type': 'application/json'}
+    resp = requests.request("POST", url, headers=headers, data=payload).json()
+
     if "result" in resp:
         return resp["result"].split("\n")[0]
     else:
